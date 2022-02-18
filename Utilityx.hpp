06a71092d87df2cba99c37e825a79617b9e1d4e0
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2022-02-18 23:48:13 +0300

    Update Utilityx.hpp

--- Utilityx.hpp
+++ Utilityx.hpp
@@ -5,7 +5,7 @@
 #include <unordered_map>
 #include <memory>
 #define rep(v, j) for (decltype(v) i = 0; i < v; i += j)
-#define once(v)   for (static bool v = true; v; v = false)
+#define once(v)   for (static boolean v = True; v; v = False)
 #define pass {}
 #define namestr(x) #x
 #define newln std::cout<<'\n';
@@ -27,27 +27,30 @@
 #define ret return
 #define to ->
 #define none NULL
+#define new_ptr(v) (new decltype(v)(v))
 #define alias using
 #define del delete
 #define op operator
 #define True true
 #define False false
-#define boolean bool
 #define randf ((float)std::rand()/(float)RAND_MAX)
 #define clambda(fn_body,...) struct { auto operator()(__VA_ARGS__){fn_body;}}
 #define dotspace(_name,body) struct { body } _name;
 alias real = float;
+alias boolean = bool;
 alias str = std::string;
-template <typename T>
-alias dict = std::unordered_map<str, T>;
+template <typename T> alias dict = std::unordered_map<str, T>;
+template <typename T> alias list = std::pmr::vector<T>;
+template <typename T> alias func_ptr = void(*)(T);
+typedef void* ptr;
 namespace std {
-	int   atoi(bool& v) { return v ? 1 : 0; };
-	float atof(bool& v) { return v ? 1.f : 0.f; };
-	bool  atob(const std::string& v) { return (v == "1" or v == "true" or v == "True") ? true : false; };
+	int  atoi(boolean& v) { ret v ? 1 : 0; };
+	real atof(boolean& v) { ret v ? 1.f : 0.f; };
+	boolean atob(const str& v) { ret (v == "1" or v == "true" or v == "True") ? True : False; };
 };
-void printx() { std::cout << '\n'; };
+fn printx() { std::cout << '\n'; };
 template <typename T, typename ...TAIL>
-void printx(const T& v, TAIL... tail) {
+fn printx(const T& v, TAIL... tail) {
 	std::cout << v;
 	printx(tail...);
 };
